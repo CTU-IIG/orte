ee372e0a5907416d1602eb37e622af44b1ccf380
Martin Vajnar <martin.vajnar@gmail.com>
2013-07-07 20:16:52 +0200

    ROBOT_DEMO: add robot demo for Android
    Initial commit of robot demo for Android.

---
+++ orte/examples/robot/subscriber.c
@@ -0,0 +1,125 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "orte.h"
+
+typedef struct motion_speed_type motion_speed;
+struct motion_speed_type {
+	CORBA_short left;
+	CORBA_short right;
+};
+
+struct robottype_orte_data {
+	ORTEDomain *orte_domain;
+	int strength;
+
+	struct motion_speed_type motion_speed;
+	ORTEPublication *publication_motion_speed;
+};
+
+struct robottype_orte_data orte;
+
+
+int robottype_roboorte_init(struct robottype_orte_data *data) {
+	int rv = 0;
+	ORTEDomainProp prop;
+	ORTEInit();
+
+	if (data->strength < 0)
+		data->strength = 1;
+
+	ORTEVerbositySetOptions("ALL.0");
+	ORTEDomainPropDefaultGet(&prop);
+	NTPTIME_BUILD(prop.baseProp.refreshPeriod, 3);
+	NTPTIME_BUILD(prop.baseProp.expirationTime, 10);
+	data->orte_domain = ORTEDomainAppCreate(ORTE_DEFAULT_DOMAIN, &prop, NULL, ORTE_FALSE);
+	if (!data->orte_domain) {
+		printf("ORTEDomainAppCreate failed!\n");
+		rv = -1;
+	}
+
+	return rv;
+}
+
+void motion_speed_serialize(CDR_Codec *cdrCodec, motion_speed *object) {
+	CORBA_short_serialize(cdrCodec,&(object->left));
+
+	CORBA_short_serialize(cdrCodec,&(object->right));
+
+}
+
+void motion_speed_deserialize(CDR_Codec *cdrCodec, motion_speed *object) {
+	CORBA_short_deserialize(cdrCodec,&(object->left));
+
+	CORBA_short_deserialize(cdrCodec,&(object->right));
+
+}
+
+int motion_speed_get_max_size(ORTEGetMaxSizeParam *gms, int num) {
+	int loop_lim=2;
+	int csize_save;
+	while(num) {
+		if (!loop_lim--) {
+			gms->csize+=num*(gms->csize-csize_save);
+			return gms->csize;
+		}
+		num--;
+		csize_save=gms->csize;
+		CORBA_short_get_max_size(gms, 1);
+		CORBA_short_get_max_size(gms, 1);
+	}
+	return gms->csize;
+}
+
+Boolean
+motion_speed_type_register(ORTEDomain *d) {
+	Boolean ret;
+
+	ret=ORTETypeRegisterAdd(d,
+				"motion_speed",
+				(ORTETypeSerialize)motion_speed_serialize,
+				(ORTETypeDeserialize)motion_speed_deserialize,
+				motion_speed_get_max_size,
+				0);
+}
+
+void robottype_subscriber_motion_speed_create(struct robottype_orte_data *data, ORTERecvCallBack callback, void *arg) {
+	ORTESubscription *s;
+	NtpTime deadline, minimumSeparation;
+
+	motion_speed_type_register(data->orte_domain);
+
+	NtpTimeAssembFromMs(deadline, 0, 300);
+	NtpTimeAssembFromMs(minimumSeparation, 0, 0);
+	s = ORTESubscriptionCreate(
+			data->orte_domain, IMMEDIATE, BEST_EFFORTS,
+			"motion_speed", "motion_speed",
+			&data->motion_speed, &deadline, &minimumSeparation,
+			callback, arg, IPADDRESS_INVALID);
+}
+
+void rcv_motion_speed_cb(const ORTERecvInfo *info, void *vinstance,
+			void *recvCallBackParam)
+{
+	struct motion_speed_type *m = (struct motion_speed_type *)vinstance;
+
+	switch (info->status) {
+		case NEW_DATA:
+			printf("Prijata data: left: %d, right: %d\n", m->left, m->right);
+			break;
+		case DEADLINE:
+			printf("ORTE deadline occurred - motion_speed receive\n");
+			break;
+	}
+}
+
+int main(void) {
+	orte.strength = 30;
+	robottype_roboorte_init(&orte);
+	robottype_subscriber_motion_speed_create(&orte, rcv_motion_speed_cb, &orte);
+
+	while (1) {
+		ORTESleepMs(1000);
+	}
+
+	return 0;
+}
---
+++ orte/java/src/org/ocera/orte/examples/robot/RoboSubscriber.java
@@ -0,0 +1,123 @@
+/* Subscriber.java  */
+
+/**
+ * Class Subscriber provides methods for creating and working
+ * with the ORTE subscriber.
+ *
+ * @author Lukas Pokorny (lukas_pokorny@centrum.cz)
+ * @author CTU FEE Prague - Department of Control Engineering (dce.felk.cvut.cz)
+ * @author Project ORTE - OCERA Real Time Ethernet (www.ocera.org)
+ * @author dedication to Kj
+ * @version 0.1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
+import org.ocera.orte.*;
+import org.ocera.orte.types.*;
+
+
+public class RoboSubscriber {
+
+    public final static int IMMEDIATE    = 0x02;
+    public final static int BEST_EFFORTS = 0x01;
+
+   /* load native library 'libjorte.so' */
+   static 
+   {
+     System.loadLibrary("jorte");      
+   }
+
+
+  public static void main(String[] args)
+  {
+    boolean b;
+
+    System.out.println(":j: * ********************************************* *"); 
+    System.out.println(":j: *         JORTE Subscriber is starting..        *");
+    System.out.println(":j: * ********************************************* *");
+    
+    RoboEvents events = new RoboEvents();
+
+    /**
+     *  Create a new Application Domain for a Subscriber.
+     * (Constructor without arguments creates the default Application Domain.)
+     */ 
+    // creates default app domain
+    //DomainApp appDomain = new DomainApp();
+    
+    // creates user defined app domain
+    DomainApp appDomain = new DomainApp(ORTEConstant.ORTE_DEFAULT_DOMAIN,
+                                        DomainProp.defaultPropsCreate(),
+                                        events,
+                                        false);	
+	if (appDomain == null) 
+    {
+      System.out.println(":!j:SUBS: cannot create application domain ");
+      return;
+    }
+        
+    // create a callback instance 
+    RoboCallback mycallback = new RoboCallback();
+    
+    // create a message instance
+    RoboMsg hellomsg = new RoboMsg(appDomain,"motion_speed");
+
+    // set subscriber's basic parameters
+    NtpTime deadline      = new NtpTime(10);
+    NtpTime minSeparation = new NtpTime(0);
+    long multicastIPAddr  = 0;
+    
+    // create basic subscription properties
+    SubsProp subProps = new SubsProp(hellomsg.getTopic(),
+    		                         "motion_speed",                       		
+			                         minSeparation,  
+			                         deadline,
+									 IMMEDIATE,
+									 BEST_EFFORTS,
+									 multicastIPAddr);
+
+    /** set verbosity mode - just uncomment the line bellow */
+    // JOrte.verbositySetOptions("ALL:10");
+
+    // create a subscription				                                             					                                             				                                             
+    Subscription sub = appDomain.createSubscription(subProps,
+	 		                                        hellomsg,
+	 		                                        mycallback);
+
+    if(sub == null) 
+    {    
+    	System.out.println(":!j: subscriber not created!");
+        return;   
+    }	
+   	System.out.println(":j: subscriber created!");
+    System.out.println(":j: * ********************************************* *");
+   	System.out.println(":j: INFO: subscriber is setted to be alive for 10 counts");	
+   	System.out.println(":j: * ********************************************* *");
+
+   	// Subscriber is alive only 10 callback calling
+	while(mycallback.cnt < 11)
+	{
+	   System.out.println(":j: count: #" + mycallback.cnt);
+	   System.out.println(">>  recieved data: " + hellomsg);	    
+	   JOrte.sleepMs(1);
+	   System.out.println(" ");
+	}
+
+	// kill & clean subscriber - call Garbage Collector 
+	sub = null;
+    System.out.println("Calling gc() ...");
+	System.gc();
+
+  }
+
+}
