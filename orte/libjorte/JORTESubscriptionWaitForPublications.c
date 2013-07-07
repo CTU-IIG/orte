829bf265c7568a9f068b7411f8cda0af33b6e6da
Martin Vajnar <martin.vajnar@gmail.com>
2013-07-07 13:19:57 +0200

    JORTE: fix typo

--- orte/libjorte/JORTESubscriptionWaitForPublications.c
+++ orte/libjorte/JORTESubscriptionWaitForPublications.c
@@ -38,7 +38,7 @@
 
 
 JNIEXPORT jint JNICALL
-Java_org_ocera_orte_Subscription_jORTESubscriptionWaitForSubscriptions
+Java_org_ocera_orte_Subscription_jORTESubscriptionWaitForPublications
 (JNIEnv *env, jobject obj, jint j_appDomain_handle,
  jobject obj_ntpT, jlong jretries, jlong jnoPublications)
 {
