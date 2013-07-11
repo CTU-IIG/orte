469582ed3e30c8e5cf25daaefc56071af0e60e5b
Martin Vajnar <martin.vajnar@gmail.com>
2013-07-11 13:46:23 +0200

    ROBOT_DEMO: use Manager class instead of ortemanager binary

--- orte/Robot_Demo/gen/org/ocera/orte/demo/BuildConfig.java
+++ orte/Robot_Demo/gen/org/ocera/orte/demo/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package org.ocera.orte;
+package org.ocera.orte.demo;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
