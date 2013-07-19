c2dc051a6d66caafe3372e5706fc7699012783f3
Martin Vajnar <martin.vajnar@gmail.com>
2013-07-19 15:32:09 +0200

    ROBOT_DEMO: be less verbose

--- orte/Robot_Demo/src/org/ocera/orte/demo/MainActivity.java
+++ orte/Robot_Demo/src/org/ocera/orte/demo/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends Activity {
     
     @Override
     protected void onDestroy() {
-    	super.onStop();
+    	super.onDestroy();
     	
         if (appDomain != null) {
         	appDomain.destroy();
