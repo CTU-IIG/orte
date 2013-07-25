575a15ce3e5110f1f69bd7b134a7f7c355c67b29
Martin Vajnar <martin.vajnar@gmail.com>
2013-07-25 19:11:47 +0200

    ROBOT_DEMO: incorporate changes in ORTETypeRegisterAdd()

--- orte/Robot_Demo/src/org/ocera/orte/demo/HokuyoScanType.java
+++ orte/Robot_Demo/src/org/ocera/orte/demo/HokuyoScanType.java
@@ -16,7 +16,7 @@ public class HokuyoScanType extends MessageData {
 	 	     boolean b;
 	 	     counter++;
 	         this.setTopic(newTopic); 
-	     	 b = domainApp.regNewDataType("hokuyo_scan",getMaxDataLength()); 
+	     	 b = domainApp.regNewDataType("hokuyo_scan",getMaxDataLength(),this.buffer); 
 		     if (b == false) {
 		       System.out.println(":j!: cannot register data type!");    
 		     }
