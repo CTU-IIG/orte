ddc356ef6788e1238435ad8303517a2fbb631b6f
Martin Vajnar <martin.vajnar@gmail.com>
2013-08-21 23:03:31 +0200

    ROBOT_DEMO: add motion speed subscriber
    Add motion speed monitor.

--- orte/Robot_Demo/gen/org/ocera/orte/demo/R.java
+++ orte/Robot_Demo/gen/org/ocera/orte/demo/R.java
@@ -14,12 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int exit=0x7f070005;
-        public static final int hokuyo=0x7f070002;
+        public static final int exit=0x7f070006;
+        public static final int hokuyo=0x7f070003;
         public static final int hokuyo_view=0x7f070000;
-        public static final int lift=0x7f070003;
-        public static final int manget=0x7f070004;
+        public static final int lift=0x7f070004;
+        public static final int magnet=0x7f070005;
         public static final int speed=0x7f070001;
+        public static final int speed_mon=0x7f070002;
     }
     public static final class layout {
         public static final int hokuyo_view=0x7f030000;
