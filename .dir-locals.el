7551a20fa187e372a91f00e02b24e862531bd1ad
Michal Sojka <sojkam1@fel.cvut.cz>
2015-02-07 13:21:22 +0100

    Add configuration for uncrustify
    The formatting of ORTE source code is messy. To unify the formatting at
    least a bit, we will use the uncrustify tool. This commit adds
    uncrustify configuration tuned for ORTE sources and a helper script that
    call uncrustify on all relevant files.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((c-mode
   (indent-tabs-mode . t)
   (tab-width . 8)
   (c-basic-offset . 2)
-  (c-file-style . "linux")))
+  (c-file-style . "linux")
+  (c-file-offsets
+   (case-label . +))))
