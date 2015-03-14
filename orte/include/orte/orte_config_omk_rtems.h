098ecfdc87d8f42d1d591c78e1be608e8b94ea43
Michal Sojka <sojkam1@fel.cvut.cz>
2015-03-14 16:51:02 +0100

    Add orte_config.h for RTEMS
    Since the previous commit, it is no longer possible to compile for Linux
    and RTEMS with the same config file.

---
+++ orte/include/orte/orte_config_omk_rtems.h
@@ -0,0 +1,308 @@
+/* orte/include/orte/orte_config.h.  Generated from orte_config.h.in by configure.  */
+/* orte/include/orte/orte_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* Define to enable JORTE's compilation */
+/* #undef CONFIG_JORTE */
+
+/* Define if is target CYGWIN */
+/* #undef CONFIG_ORTE_CYGWIN */
+
+/* Define if linux kernel is found */
+/* #undef CONFIG_ORTE_KERNEL */
+
+/* Define if is target MINGW */
+/* #undef CONFIG_ORTE_MINGW */
+
+/* Define to enable ORTE's RT support */
+/* #undef CONFIG_ORTE_RT */
+
+/* Define if kernel is RTAI patched */
+/* #undef CONFIG_ORTE_RTAI */
+
+/* Define if kernel is RTLinux patched */
+/* #undef CONFIG_ORTE_RTL */
+
+/* Define if onetd stack is found */
+/* #undef CONFIG_ORTE_RTL_ONETD */
+
+/* Define if wIP stack is found */
+/* #undef CONFIG_ORTE_RTL_WIP */
+
+/* Define if is target a unix system */
+#define CONFIG_ORTE_UNIX 1
+
+/* Define if is target windows */
+/* #undef CONFIG_ORTE_WIN */
+
+/* Define to 1 if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
+/* Define to 1 if you have the <asm/byteorder.h> header file. */
+/* #undef HAVE_ASM_BYTEORDER_H */
+
+/* Define to 1 if you have the <byteswap.h> header file. */
+/* #define HAVE_BYTESWAP_H 1 */
+
+/* Define to 1 if you have the <ctype.h> header file. */
+#define HAVE_CTYPE_H 1
+
+/* Define to 1 if you have the declaration of `CLOCK_MONOTONIC', and to 0 if
+   you don't. */
+#define HAVE_DECL_CLOCK_MONOTONIC 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <endian.h> header file. */
+#define HAVE_ENDIAN_H 1
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `gethostbyname' function. */
+#define HAVE_GETHOSTBYNAME 1
+
+/* Define to 1 if you have the <getopt.h> header file. */
+#define HAVE_GETOPT_H 1
+
+/* Define to 1 if you have the `getopt_long' function. */
+#define HAVE_GETOPT_LONG 1
+
+/* Define for ORTE getopt_long self implemetation */
+/* #undef HAVE_GETPOT_LONG_ORTE */
+
+/* Define to 1 if you have the `gettimeofday' function. */
+/* #undef HAVE_GETTIMEOFDAY */
+
+/* Define to 1 if you have the <ifaddrs.h> header file. */
+#define HAVE_IFADDRS_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `c_r' library (-lc_r). */
+/* #undef HAVE_LIBC_R */
+
+/* Define to 1 if you have the `nsl' library (-lnsl). */
+#define HAVE_LIBNSL 1
+
+/* Define to 1 if you have the `popt' library (-lpopt). */
+/* #undef HAVE_LIBPOPT */
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
+/* Define to 1 if you have the `rt' library (-lrt). */
+#define HAVE_LIBRT 1
+
+/* Define to 1 if you have the `socket' library (-lsocket). */
+/* #undef HAVE_LIBSOCKET */
+
+/* Define to 1 if you have the `ws2_32' library (-lws2_32). */
+/* #undef HAVE_LIBWS2_32 */
+
+/* Define to 1 if you have the <linux/ctype.h> header file. */
+/* #undef HAVE_LINUX_CTYPE_H */
+
+/* Define to 1 if you have the <linux/if.h> header file. */
+/* #undef HAVE_LINUX_IF_H */
+
+/* Define to 1 if you have the <linux/in.h> header file. */
+/* #undef HAVE_LINUX_IN_H */
+
+/* Define to 1 if you have the <linux/module.h> header file. */
+/* #undef HAVE_LINUX_MODULE_H */
+
+/* Define to 1 if you have the <linux/socket.h> header file. */
+/* #undef HAVE_LINUX_SOCKET_H */
+
+/* Define to 1 if you have the <linux/string.h> header file. */
+/* #undef HAVE_LINUX_STRING_H */
+
+/* Define to 1 if you have the <linux/time.h> header file. */
+/* #undef HAVE_LINUX_TIME_H */
+
+/* Define to 1 if you have the <linux/types.h> header file. */
+/* #undef HAVE_LINUX_TYPES_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <netdb.h> header file. */
+#define HAVE_NETDB_H 1
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have the <net/if.h> header file. */
+#define HAVE_NET_IF_H 1
+
+/* Define to 1 if you have the <nictab.h> header file. */
+/* #undef HAVE_NICTAB_H */
+
+/* Define to 1 if you have the <nic.h> header file. */
+/* #undef HAVE_NIC_H */
+
+/* Define to 1 if you have the <onetd.h> header file. */
+/* #undef HAVE_ONETD_H */
+
+/* Define to 1 if you have the `pthread_condattr_setclock' function. */
+/* #define HAVE_PTHREAD_CONDATTR_SETCLOCK 1 */
+
+/* Define to 1 if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 1
+
+/* Define to 1 if you have the <rtl.h> header file. */
+/* #undef HAVE_RTL_H */
+
+/* Define to 1 if you have the <rtl_malloc.h> header file. */
+/* #undef HAVE_RTL_MALLOC_H */
+
+/* Define to 1 if you have the <rtnet.h> header file. */
+/* #undef HAVE_RTNET_H */
+
+/* Define to 1 if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H 1
+
+/* Define to 1 if you have the `sigwaitinfo' function. */
+#define HAVE_SIGWAITINFO 1
+
+/* Define to 1 if you have the <stdarg.h> header file. */
+#define HAVE_STDARG_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if `sa_len' is a member of `struct sockaddr'. */
+/* #undef HAVE_STRUCT_SOCKADDR_SA_LEN */
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define to 1 if you have the <sys/sockio.h> header file. */
+/* #undef HAVE_SYS_SOCKIO_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <time.h> header file. */
+/* #undef HAVE_TIME_H */
+
+/* Define to 1 if you have the <udp.h> header file. */
+/* #undef HAVE_UDP_H */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <w32api/windows.h> header file. */
+/* #undef HAVE_W32API_WINDOWS_H */
+
+/* Define to 1 if you have the <windows.h> header file. */
+/* #undef HAVE_WINDOWS_H */
+
+/* Define to 1 if you have the <winsock2.h> header file. */
+/* #undef HAVE_WINSOCK2_H */
+
+/* Define to 1 if you have the <ws2tcpip.h> header file. */
+/* #undef HAVE_WS2TCPIP_H */
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define ORTE_PACKAGE_BUGREPORT "orte@rtime.felk.cvut.cz"
+
+/* Define to the full name of this package. */
+#define ORTE_PACKAGE_NAME "orte"
+
+/* Define to the full name and version of this package. */
+#define ORTE_PACKAGE_STRING "orte 0.3.4"
+
+/* Define to the one symbol short name of this package. */
+#define ORTE_PACKAGE_TARNAME "orte"
+
+/* Define to the home page for this package. */
+#define ORTE_PACKAGE_URL "http://orte.sf.net"
+
+/* Define to the version of this package. */
+#define ORTE_PACKAGE_VERSION "0.3.4"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+
+
+#ifndef _GNU_SOURCE
+  #define _GNU_SOURCE
+#endif
+
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+
+#if defined(HAVE_ENDIAN_H) || defined(HAVE_SYS_PARAM_H)
+  #ifdef HAVE_SYS_PARAM_H
+    #include <sys/param.h>
+  #endif
+  #if defined(HAVE_ENDIAN_H) && !defined(BYTE_ORDER) && !defined(__BYTE_ORDER)
+    #include <endian.h>
+  #endif
+  #if defined(__BYTE_ORDER)
+    #if __BYTE_ORDER == __BIG_ENDIAN
+      #define WORDS_BIGENDIAN 1
+    #endif
+  #elif defined(BYTE_ORDER)
+    #if BYTE_ORDER == BIG_ENDIAN
+      #define WORDS_BIGENDIAN 1
+    #endif
+  #endif
+#endif
